aaa41b1f2f9736002f910edd9ba3bf2208d2ded0
IncognitoAnkh <angelvalletm@gmail.com>
2022-10-15 01:29:15 +0000

    Analytics integration

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -4,5 +4,4 @@ const mainMenuElement = document.getElementById('navbar_buttons');
 toggleMenuElement.addEventListener('click', () => {
     mainMenuElement.classList.toggle('menu_show');
     toggleMenuElement.classList.toggle('highlight');
-    console.log('working');
 });
