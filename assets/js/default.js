897f09f65570c110f77c991fee7a2eea051b5994
IncognitoAnkh <angelvalletm@gmail.com>
2023-07-26 10:42:48 +0000

    New error 404 variations

--- assets/js/default.js
+++ assets/js/default.js
@@ -2,6 +2,6 @@ window.onload = ImageRandom;
 imagesArray = ["/assets/images/MLOPLN.png", "/assets/images/Bruno.png", "/assets/images/MondoParado.png", "/assets/images/MLOPLN2.png"];
 function ImageRandom(){
 
-     var num = Math.floor(Math.random() * 3);
+     var num = Math.floor(Math.random() * 4);
      document.getElementById("RandomImage").src = imagesArray[num];
 }
