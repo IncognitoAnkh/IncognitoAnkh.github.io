aa4230665d8207b5404e42cee33f402e108f446e
IncognitoAnkh <angelvalletm@gmail.com>
2023-12-29 13:43:54 -0600

    Big Update

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,7 +1,7 @@
 window.onload = ImageRandom;
-imagesArray = ["/assets/images/MLOPLN.png", "/assets/images/Bruno.png", "/assets/images/MondoParado.png", "/assets/images/MLOPLN2.png"];
+imagesArray = ["/assets/images/MLOPLN.png", "/assets/images/Bruno.png", "/assets/images/MondoParado.png", "/assets/images/MLOPLN2.png", "/assets/images/yobikomikun.gif"];
 function ImageRandom(){
 
-     var num = Math.floor(Math.random() * 4);
+     var num = Math.floor(Math.random() * 5);
      document.getElementById("RandomImage").src = imagesArray[num];
 }
